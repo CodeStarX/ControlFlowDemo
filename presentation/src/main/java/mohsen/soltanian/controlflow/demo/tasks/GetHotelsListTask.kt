1c49787b13a4c76c6b25719ba19dc8984455ce69
mohsen <soltaniyan.mohsen@gmail.com>
2024-01-03 17:05:40 +0330

    feat(:presentation): activate the retry mechanism for AuthorizationTask and GetHotelsListTask

--- presentation/src/main/java/mohsen/soltanian/controlflow/demo/tasks/GetHotelsListTask.kt
+++ presentation/src/main/java/mohsen/soltanian/controlflow/demo/tasks/GetHotelsListTask.kt
@@ -5,12 +5,14 @@ import io.github.codestarx.helper.failureMode
 import io.github.codestarx.helper.successMode
 import io.github.codestarx.interfaces.TaskProcessor
 import io.github.codestarx.models.ConditionData
+import io.github.codestarx.models.RetryStrategy
 import io.github.codestarx.models.TaskInfo
 import io.github.codestarx.status.TaskStatus
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import mohsen.soltanian.controlflow.demo.core.domain.base.BaseUseCase
 import mohsen.soltanian.controlflow.demo.core.domain.usecases.GetHotelsUseCase
+import java.util.concurrent.TimeoutException
 
 
 class GetHotelsListTask(
@@ -20,6 +22,11 @@ class GetHotelsListTask(
         get() = TaskInfo().apply {
             index = 2
             name = GetHotelsListTask::class.java.name
+            retry = RetryStrategy().apply {
+                count = 3
+                causes = setOf(TimeoutException::class)
+                delay = 1000L
+            }
             runIn = Dispatchers.IO
         }
 
